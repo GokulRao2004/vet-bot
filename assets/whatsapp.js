df568c5644b8252ae2f5bfc3523fbf8a6b5cc2ca
gokul-the-developer <62870769+gokul-the-developer@users.noreply.github.com>
2024-07-06 14:08:46 +0530

    before adding message icon in sidebar

---
+++ assets/whatsapp.js
@@ -0,0 +1,35 @@
+import pp from "./whatsapp/pp.png";
+import cs1 from "./whatsapp/cs1.png";
+import cs2 from "./whatsapp/cs2.jpeg";
+import chat1 from "./whatsapp/chat1.png";
+import chat2 from "./whatsapp/chat2.png";
+import chat3 from "./whatsapp/chat3.png";
+import chat4 from "./whatsapp/chat4.png";
+import chat5 from "./whatsapp/chat5.png";
+import chat6 from "./whatsapp/chat6.png";
+import chat7 from "./whatsapp/chat7.png";
+import chat8 from "./whatsapp/chat8.png";
+import chat9 from "./whatsapp/chat9.png";
+import chat10 from "./whatsapp/chat10.png";
+import chat11 from "./whatsapp/chat11.png";
+import chat12 from "./whatsapp/chat12.png";
+import chat13 from "./whatsapp/chat13.png";
+
+export {
+  pp,
+  cs1,
+  cs2,
+  chat1,
+  chat2,
+  chat3,
+  chat4,
+  chat5,
+  chat6,
+  chat7,
+  chat8,
+  chat9,
+  chat10,
+  chat11,
+  chat12,
+  chat13,
+};
