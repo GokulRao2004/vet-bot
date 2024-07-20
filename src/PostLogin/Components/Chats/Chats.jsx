fe7ba888f3a1df5c3f63530714060f99a3a4aa6d
gokul-the-developer <62870769+gokul-the-developer@users.noreply.github.com>
2024-07-20 20:03:01 +0530

    chats page updated

--- src/PostLogin/Components/Chats/Chats.jsx
+++ src/PostLogin/Components/Chats/Chats.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Chats.module.css'
-import { ArrowBackRounded } from '@mui/icons-material'
+import { ArrowBackRounded, AttachFile, Attachment, Send } from '@mui/icons-material'
 import { useNavigate } from 'react-router-dom'
 import { getImageUrl } from '../../../utils'
 
 export const Chats = ({ name }) => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [message, setMessage] = useState('');
+
+    const handleSendMessage = () => {
+        if (message.trim() !== '') {
+            
+            setMessage('');
+        }
+    };
+
+
     const goBack = () => {
         navigate("../whatsapp")
     };
+
+    const handleFileAttach = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+          console.log('File attached:', file);
+          // Handle the file attachment logic here
+        }
+      };
 
 
     return (
         <div className={styles.container}>
             <div className={styles.navbar}>
                 <a onClick={goBack} style={{ cursor: 'pointer' }}>
-                    <ArrowBackRounded sx={{margin: 0, padding:0}}/>
+                    <ArrowBackRounded sx={{ margin: 0, padding: 0 }} />
                 </a>
                 <div className={styles.profilePic}>
                     <img src={getImageUrl("img.jpeg")} alt="profile" />
@@ -23,9 +41,29 @@ export const Chats = ({ name }) => {
                 <div>{name}</div>
             </div>
             <div className={styles.messages}>
-                
+
             </div>
-            <div className={styles.chat}>
+            <div className={styles.chatBar}>
+                <label htmlFor="file-input" className={styles.fileAttachLabel} style={{margin:0, padding:0}}>
+                    <AttachFile/>
+                </label>
+                <input
+                    type="file"
+                    id="file-input"
+                    className={styles.fileInput}
+                    onChange={handleFileAttach}
+                />
+                <input
+                    type="text"
+                    className={styles.chatInput}
+                    placeholder="Type a message..."
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                
+                    <button className={styles.sendBtn} onClick={handleSendMessage}>
+                        <Send />
+                    </button>
                 
             </div>
         </div>
