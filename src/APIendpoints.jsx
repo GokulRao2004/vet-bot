ec5f962a44e437438d70239c72cf5b9d02b58925
gokul-the-developer <62870769+gokul-the-developer@users.noreply.github.com>
2024-08-17 18:52:57 +0530

    added backend_path

--- src/APIendpoints.jsx
+++ src/APIendpoints.jsx
@@ -1,10 +1,10 @@
 import config from './config.json';
 
 const apiBaseUrl = config.apiBaseUrl1;
-
+const backend_path = config.backend_path;
 const endpoints = {
-  login: `${apiBaseUrl}${config.endpoints.login}`,
-  verifyToken: `${apiBaseUrl}${config.endpoints.verifyToken}`,
+  login: `${apiBaseUrl}${backend_path}${config.endpoints.login}`,
+  verifyToken: `${apiBaseUrl}${backend_path}${config.endpoints.verifyToken}`,
   whatsappLogin : `${apiBaseUrl}${config.endpoints.whatsappLogin}`
 };
 
